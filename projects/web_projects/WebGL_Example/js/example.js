50c9e64b14bf691af13dfc33440da030cde79335
폴로늄홍차 <54669769+ITisIT@users.noreply.github.com>
2021-10-28 09:31:44 +0900

    2021-10-28 09:31 KST

---
+++ projects/web_projects/WebGL_Example/js/example.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    // wrapMgnb
+    $(".mmenu").on("click", function() {
+        $(".wrapMgnb").stop().slideToggle();
+    });
+
+
+    // scroll trigger
+    $(window).scroll(function () {
+        $(".ani_stop").each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() * .3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if (bottom_of_window > bottom_of_object) {
+                $(this).removeClass("ani_stop");
+            }
+            if (bottom_of_window < bottom_of_object) {
+                $(this).addClass("ani_stop");
+            }
+        });
+    });
+});
